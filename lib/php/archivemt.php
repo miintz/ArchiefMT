e8bfc472b2288c3ae552e8dbe3d520b23dc702b4
miintz <vanheesmaarten@hotmail.com>
2015-11-15 15:40:47 +0100

    maarten: skeleton of the structure

---
+++ lib/php/archivemt.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+
+Archive for MediaTech. The limited API that enables the embedding of a project browser in the MediaTech
+website. 
+
+Authors: 
+Maarten van Hees
+Roland van Dierendonck
+Max van Duijn
+
+VER: 0.01
+
+*/
+	
+echo "Hi, this is not working quite yet."
+
+$instance = new ArchiveMT();
+
+public class ArchiveMT
+{
+	__construct($args = "")
+	{
+		//do something here...
+	}
+	
+	/**
+	*	Get projects from database based on user constraints
+	*/
+	function getProjects($basedon)
+	{
+		return null;
+	}
+	
+	/**
+	*	Get people from database based on user constraints
+	*/
+	function getPeople($basedon)
+	{
+		return null;
+	}
+	
+	/**
+	*	Get courses from database based on user constraints
+	*/
+	function getCourses($basedon)
+	{
+		return null;
+	}
+}
+
+?>
