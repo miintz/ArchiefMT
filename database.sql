c021f2271cee7fb49a5aa9a5ae0bdaf734f40c06
miintz <vanheesmaarten@hotmail.com>
2015-11-15 15:16:21 +0100

    maarten: initial database setup

---
+++ database.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Machine: 127.0.0.1
+-- Gegenereerd op: 15 nov 2015 om 15:15
+-- Serverversie: 5.6.16
+-- PHP-versie: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Databank: `mtarchive`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `courses`
+--
+
+CREATE TABLE IF NOT EXISTS `courses` (
+  `id` int(11) NOT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `people` int(11) NOT NULL,
+  `crdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tstamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='table for course information';
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `media`
+--
+
+CREATE TABLE IF NOT EXISTS `media` (
+  `id` int(11) NOT NULL,
+  `oid` int(11) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `url` varchar(45) DEFAULT NULL,
+  `crdate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `tstamp` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for projectmedia';
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `projects`
+--
+
+CREATE TABLE IF NOT EXISTS `projects` (
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tstamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `crdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `title` varchar(45) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `media` varchar(45) DEFAULT NULL,
+  `people` varchar(45) DEFAULT NULL,
+  `course` int(11) DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `id_UNIQUE` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for project entries, contains links to courses, media and people(?)' AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
