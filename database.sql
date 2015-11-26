a64137c6124e4b98e4c8cc851e4ffe183f80381d
Maarten van Hees <maarten@lingewoud.com>
2015-11-26 15:57:54 +0100

    maarten: sample

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
--- Machine: 127.0.0.1
--- Gegenereerd op: 15 nov 2015 om 15:22
--- Serverversie: 5.6.16
--- PHP-versie: 5.5.11
+-- Host: 127.0.0.1
+-- Gegenereerd op: 26 nov 2015 om 15:56
+-- Serverversie: 5.6.24
+-- PHP-versie: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Databank: `mtarchive`
+-- Database: `mtarchive`
 --
 
 -- --------------------------------------------------------
@@ -32,9 +32,16 @@ CREATE TABLE IF NOT EXISTS `courses` (
   `description` varchar(45) DEFAULT NULL,
   `people` int(11) NOT NULL,
   `crdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `tstamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `tstamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='table for course information';
+
+--
+-- Gegevens worden geëxporteerd voor tabel `courses`
+--
+
+INSERT INTO `courses` (`id`, `title`, `description`, `people`, `crdate`, `tstamp`) VALUES
+(0, 'New Media New Technology', 'Description about the course', 0, '2015-11-26 00:00:00', '2015-11-26 00:00:00'),
+(1, 'Human Computer Interaction', 'Description about hci', 1, '2015-11-26 00:00:00', '2015-11-26 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -48,8 +55,7 @@ CREATE TABLE IF NOT EXISTS `media` (
   `type` varchar(45) DEFAULT NULL,
   `url` varchar(45) DEFAULT NULL,
   `crdate` datetime DEFAULT CURRENT_TIMESTAMP,
-  `tstamp` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `tstamp` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for projectmedia';
 
 -- --------------------------------------------------------
@@ -63,9 +69,16 @@ CREATE TABLE IF NOT EXISTS `people` (
   `title` varchar(45) DEFAULT NULL,
   `description` varchar(45) DEFAULT NULL,
   `tstamp` datetime DEFAULT NULL,
-  `crdate` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `crdate` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='table for some info about people';
+
+--
+-- Gegevens worden geëxporteerd voor tabel `people`
+--
+
+INSERT INTO `people` (`id`, `title`, `description`, `tstamp`, `crdate`) VALUES
+(0, 'Peter vd Putten', 'Lecturer for NMNT', '2015-11-26 00:00:00', '2015-11-26 00:00:00'),
+(1, 'Fons Verbeek', 'Lecturer for HCI', '2015-11-26 00:00:00', '2015-11-26 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -74,18 +87,63 @@ CREATE TABLE IF NOT EXISTS `people` (
 --
 
 CREATE TABLE IF NOT EXISTS `projects` (
-  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(10) unsigned NOT NULL,
   `tstamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `crdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `title` varchar(45) NOT NULL,
   `description` varchar(45) DEFAULT NULL,
   `media` varchar(45) DEFAULT NULL,
   `people` varchar(45) DEFAULT NULL,
-  `course` int(11) DEFAULT NULL,
-  PRIMARY KEY (`uid`),
-  UNIQUE KEY `id_UNIQUE` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Table for project entries, contains links to courses, media and people(?)' AUTO_INCREMENT=1 ;
+  `course` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COMMENT='Table for project entries, contains links to courses, media and people(?)';
 
+--
+-- Gegevens worden geëxporteerd voor tabel `projects`
+--
+
+INSERT INTO `projects` (`uid`, `tstamp`, `crdate`, `title`, `description`, `media`, `people`, `course`) VALUES
+(1, '2015-11-26 00:00:00', '2015-11-26 00:00:00', 'Nostalgia Machine', 'A machine for nostalgic feelings', '', '0', 1),
+(2, '2015-11-26 00:00:00', '2015-11-26 00:00:00', 'Touch Table for traffic', 'Traffic system', '', '0', 1),
+(3, '2015-11-26 00:00:00', '2015-11-26 00:00:00', 'Tender', 'Analog for dating sites', '', '0', 1),
+(4, '2015-11-26 00:00:00', '2015-11-26 00:00:00', 'Project', 'Analog for dating sites', '', '0', 1);
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `media`
+--
+ALTER TABLE `media`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `people`
+--
+ALTER TABLE `people`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`uid`), ADD UNIQUE KEY `id_UNIQUE` (`uid`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
