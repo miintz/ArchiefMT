2064211a1868569f035a0bd49db1374ca4fa091b
miintz <vanheesmaarten@hotmail.com>
2016-03-09 13:24:10 +0100

    basic masonry & infinte scroll

---
+++ wp/wp-content/plugins/ajax-load-more/core/repeater/default.php
@@ -0,0 +1,35 @@
+<article id="post-<?php the_id(); ?>" class="post-<?php the_id(); ?> post type-post status-publish format-standard hentry category-"> 
+	<header class="entry-header">
+    	<h2 class="entry-title">
+        	<a href="<?php the_guid(); ?>" rel="bookmark"><?php the_title(); ?></a>
+    	</h2>
+	</header><!-- .entry-header -->		
+	<div class="entry-content">
+    	<p>
+        <img class="alignnone" src="" alt="" width="200" height="265">
+    	</p>
+	</div><!-- .entry-content -->	
+<footer class="entry-footer">
+				<span class="byline">
+					<span class="author vcard">
+						<img alt="" src="http://0.gravatar.com/avatar/9bebaa8ac9e8565bcc3b0603b362f810?s=49&amp;d=mm&amp;r=g" srcset="http://0.gravatar.com/avatar/9bebaa8ac9e8565bcc3b0603b362f810?s=98&amp;d=mm&amp;r=g 2x" class="avatar avatar-49 photo" height="49" width="49">
+						<span class="screen-reader-text">Author </span> 
+						<a class="url fn n" href="http://localhost/archiefMT/wp/author/root/">root
+						</a>
+					</span>
+				</span>
+				<span class="posted-on">
+					<span class="screen-reader-text">Posted on </span>
+					<a href="http://localhost/archiefMT/wp/2016/03/09/michael-laudrup/" rel="bookmark"><time class="entry-date published updated" datetime="2016-03-09T11:54:11+00:00">March 9, 2016</time></a>
+				</span>
+				<span class="comments-link">
+					<a href="http://localhost/archiefMT/wp/2016/03/09/michael-laudrup/#respond">Leave a comment
+						<span class="screen-reader-text"> on Michael Laudrup</span>
+					</a>
+				</span>		
+				<span class="edit-link"><a class="post-edit-link" href="http://localhost/archiefMT/wp/wp-admin/post.php?post=42&amp;action=edit">Edit
+					<span class="screen-reader-text"> "Michael Laudrup"</span>
+				</a>
+			</span>	
+		</footer><!-- .entry-footer -->
+</article>
